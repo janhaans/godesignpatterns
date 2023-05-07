ad442b4ab023302921059e9a53980a1a90514abc
Jan Haans <jan.haans@gmail.com>
2023-05-07 18:37:57 +0200

    Added FiletrBySize method

--- ocp/main.go
+++ ocp/main.go
@@ -35,16 +35,30 @@ func (f *Filter) FilterByColor(products []Product, color Color) []Product {
 	return result
 }
 
+func (f *Filter) FilterBySize(products []Product, size Size) []Product {
+	result := make([]Product, 0)
+	for _, v := range products {
+		if v.size == size {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
 func main() {
 	apple := Product{"Apple", green, small}
-	tree := Product{"Tree", red, medium}
+	tree := Product{"Tree", green, medium}
 	house := Product{"House", blue, large}
 
 	products := []Product{apple, tree, house}
 	f := Filter{}
 
 	for _, v := range f.FilterByColor(products, green) {
-		fmt.Printf("Product %s is green", v.name)
+		fmt.Printf("Product %s is green\n", v.name)
+	}
+
+	for _, v := range f.FilterBySize(products, large) {
+		fmt.Printf("Product %s is large\n", v.name)
 	}
 
 }
